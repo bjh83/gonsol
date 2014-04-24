eb32e9730f6ddf1dd3c0fd17e018922416d13516
Brendan Higgins <bjh83@case.edu>
2014-04-24 15:38:05 -0400

    I added the code for rendering the board.

---
+++ C/show_board.c
@@ -0,0 +1,11 @@
+#include <curses.h>
+
+void draw_board(char board[][], int width, int height) {
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            move(y, x);
+            addch(board[y][x]);
+        }
+    }
+    move(height, 0);
+}
